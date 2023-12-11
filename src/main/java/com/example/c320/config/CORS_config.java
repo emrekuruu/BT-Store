fb23faacf242e66eb0360348e1a9230d762bef45
Emre Kuru <emre.kuru@ozu.edu.tr>
2023-12-11 18:18:41 +0300

    feat: Config for Frontend

---
+++ src/main/java/com/example/c320/config/CORS_config.java
@@ -0,0 +1,23 @@
+package com.example.c320.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CORS_config {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000"); // React's default port
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
+
