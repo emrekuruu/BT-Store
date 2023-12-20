f21e5f178ecf92749db77f8e4f67496956a5f23c
Emre Kuru <emre.kuru@ozu.edu.tr>
2023-12-20 13:27:30 +0300

    refactor

--- src/test/java/com/example/c320/C320ApplicationTests.java
+++ src/test/java/com/example/c320/C320ApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.c320;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class C320ApplicationTests {
     @Test
     void contextLoads() {
