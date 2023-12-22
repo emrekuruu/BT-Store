61b4d164d76f2bf8a3faedcca879ab764f218c4d
Alperen <alperen.ozturk@ozu.edu.tr>
2023-12-22 14:30:32 +0300

    2 more SystemTest method for basket added

--- src/test/java/com/example/c320/SystemTests/BasketTests.java
+++ src/test/java/com/example/c320/SystemTests/BasketTests.java
@@ -24,10 +24,12 @@ public class BasketTests {
     @Autowired
     private MongoTemplate mongoTemplate;
     @Autowired
-    private ArtistService basketService;
+    private BasketService basketService;
+    private UserService userService;
     private static final DockerImageName MONGO_IMAGE = DockerImageName.parse("mongo:4.4.2");
     private static MongoDBContainer mongoDBContainer;
-}
+
+
 
     @BeforeAll
     static void setup() {
@@ -61,7 +63,7 @@ public class BasketTests {
 
         basketService.createBasket(basket);
 
-        Basked found = basketService.getArtistById("1234").orElse(null);
+        Basket found = basketService.getBasketById("1234").orElse(null);
         assertNotNull(found, "Basket should be found in the database");
     }
 
@@ -72,14 +74,46 @@ public class BasketTests {
         // Create basket to assign
         Basket basket = new Basket();
         basket.setId("1234");
-        basket.setUserID("123");
+        basket.setUserID("12");
 
-        Basket found = basketService.getArtistById("1234").orElse(null);
-        assertNotNull(found, "Basket should not be found in the database");
+        Basket found = basketService.getBasketById("1234").orElse(null);
+        assertNull(found, "Basket should not be found in the database");
     }
 
     @Test
-    public void testAddAlreadyAddedPainting(){}
+    public void testDontAddAlreadyAddedPainting(){
+        // Create User to be assigned
+        User user = new User();
+        user.setId("12");
+        // Create User to be assigned
+        Painting painting = new Painting();
+        painting.setId("123");
+        Basket basket = new Basket();
+        basket.setId("1234");
+        user.setBasket(basket);
+        userService.addPaintingToBasket("12","123");
+        userService.addPaintingToBasket("12","123"); // Should throw a message that the painting is already in basket.
+    }
+    @Test
+    public void testAddPaintingIntoBasketandRetrievePaintings(){
+        // Create User to be assigned
+        User user = new User();
+        user.setId("12");
+        // Create User to be assigned
+        Painting painting = new Painting();
+        painting.setId("123");
+        Basket basket = new Basket();
+        basket.setId("1234");
+        user.setBasket(basket);
+        userService.addPaintingToBasket("12","123");
+
+        Basket found = basketService.getBasketById("1234").orElse(null);
+        assertNotNull(found, "Basket should be found in the database");
+    }
+
+}
+
+
 
 
 
