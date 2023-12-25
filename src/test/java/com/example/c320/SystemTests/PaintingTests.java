910ae958d96335769bdd1589f6c7eea5155919ed
Alperen <alperen.ozturk@ozu.edu.tr>
2023-12-25 15:54:07 +0300

    Test if painting features are edited correctly

--- src/test/java/com/example/c320/SystemTests/PaintingTests.java
+++ src/test/java/com/example/c320/SystemTests/PaintingTests.java
@@ -7,6 +7,8 @@ import com.example.c320.Services.PurchaseService;
 import com.example.c320.Services.UserService;
 import com.example.c320.Entities.Painting;
 import com.example.c320.Services.PaintingService;
+import com.example.c320.Entities.Artist;
+import com.example.c320.Services.ArtistService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,6 +31,7 @@ public class PaintingTests {
     private MongoTemplate mongoTemplate;
     @Autowired
     private PaintingService paintingService;
+    private ArtistService artistService;
     private static final DockerImageName MONGO_IMAGE = DockerImageName.parse("mongo:4.4.2");
     private static MongoDBContainer mongoDBContainer;
 
@@ -48,6 +51,25 @@ public class PaintingTests {
     static void tearDown() {
         if (mongoDBContainer != null) {
             mongoDBContainer.stop();
+        }
+    }
+
+    @Test
+    public void testIfPriceEditedCorrectly(){
+        Painting painting = new Painting();
+        painting.setId("123");
+        painting.setPrice(10.);
+        painting.setName("First Painting");
+        Artist artist = new Artist();
+        artist.setId("12");
+        artistService.addPainting(painting,"12");
+        Painting uptadePainting = new Painting();
+        uptadePainting.setPrice(20.);
+        uptadePainting.setId("124");
+        uptadePainting.setName("updated");
+        paintingService.updatePainting("123",uptadePainting);
+        if(paintingService.getPaintingById("123").get().getPrice() == 20.){
+            //Works corretly
         }
     }
 }
